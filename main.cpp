473417ffd0db077b94c0f239dde6e89903508bd8
Vrushali1512 <satawalekarvrushali@gmail.com>
2025-09-22 23:42:41 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,29 +1,27 @@
 #include <stdio.h>
 #include <assert.h>
-#include "color_coder.h"
+#include "color_code.h"
 
-// Test function to check number-to-pair conversion
 void testNumberToPair(int pairNumber, MajorColor expectedMajor, MinorColor expectedMinor) {
-    TelCoColorCoder_ColorPair colorPair = TelCoColorCoder_GetColorFromPairNumber(pairNumber);
-    assert(colorPair.majorColor == expectedMajor && colorPair.minorColor == expectedMinor);
-    printf("Test for pair number %d passed.\n", pairNumber);
+    ColorPair colorPair = GetColorFromPairNumber(pairNumber);
+    char colorPairStr[20];
+    ToString(colorPair, colorPairStr, sizeof(colorPairStr));
+    printf("Got pair %s\n", colorPairStr);
+    assert(colorPair.major == expectedMajor);
+    assert(colorPair.minor == expectedMinor);
 }
 
-// Test function to check pair-to-number conversion
 void testPairToNumber(MajorColor major, MinorColor minor, int expectedPairNumber) {
-    int pairNumber = TelCoColorCoder_GetPairNumberFromColor(major, minor);
+    int pairNumber = GetPairNumberFromColor(major, minor);
+    printf("Got pair number %d\n", pairNumber);
     assert(pairNumber == expectedPairNumber);
-    printf("Test for color pair %s %s passed.\n",
-           TelCoColorCoder_MajorColorNames[major],
-           TelCoColorCoder_MinorColorNames[minor]);
 }
 
 int main() {
     testNumberToPair(4, WHITE, BROWN);
     testNumberToPair(5, WHITE, SLATE);
-    
     testPairToNumber(BLACK, ORANGE, 12);
     testPairToNumber(VIOLET, SLATE, 25);
-    
+    printf("All tests passed.\n");
     return 0;
 }
