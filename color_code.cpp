f7d7caa4c7b0ebe3b3093f87dc41804651b8c83d
Vrushali1512 <satawalekarvrushali@gmail.com>
2025-09-30 15:21:38 +0530

    Update color_code.cpp

--- color_code.cpp
+++ color_code.cpp
@@ -1,27 +1,57 @@
-#include <stdio.h>
 #include "color_code.h"
+#include <sstream>
+#include <stdexcept>
 
-const char* MajorColorNames[NUMBER_OF_MAJOR_COLORS] = {
+namespace ColorCode {
+
+const std::vector<std::string> majorColors = {
     "White", "Red", "Black", "Yellow", "Violet"
 };
 
-const char* MinorColorNames[NUMBER_OF_MINOR_COLORS] = {
+const std::vector<std::string> minorColors = {
     "Blue", "Orange", "Green", "Brown", "Slate"
 };
 
 ColorPair GetColorFromPairNumber(int pairNumber) {
-    int zeroBasedPairNumber = pairNumber - 1;
-    ColorPair colorPair;
-    colorPair.major = (MajorColor)(zeroBasedPairNumber / NUMBER_OF_MINOR_COLORS);
-    colorPair.minor = (MinorColor)(zeroBasedPairNumber % NUMBER_OF_MINOR_COLORS);
-    return colorPair;
+    if (pairNumber < 1 || pairNumber > numberOfColorPairs) {
+        throw std::out_of_range("Invalid pair number");
+    }
+    int zeroBasedPair = pairNumber - 1;
+    int majorIndex = zeroBasedPair / numberOfMinorColors;
+    int minorIndex = zeroBasedPair % numberOfMinorColors;
+    return ColorPair{majorColors[majorIndex], minorColors[minorIndex]};
 }
 
-int GetPairNumberFromColor(MajorColor major, MinorColor minor) {
-    return major * NUMBER_OF_MINOR_COLORS + minor + 1;
+int GetPairNumberFromColor(const ColorPair& colorPair) {
+    int majorIndex = -1;
+    int minorIndex = -1;
+    for (int i = 0; i < numberOfMajorColors; ++i) {
+        if (majorColors[i] == colorPair.majorColor) {
+            majorIndex = i;
+            break;
+        }
+    }
+    for (int i = 0; i < numberOfMinorColors; ++i) {
+        if (minorColors[i] == colorPair.minorColor) {
+            minorIndex = i;
+            break;
+        }
+    }
+    if (majorIndex == -1 || minorIndex == -1) {
+        throw std::invalid_argument("Invalid color names");
+    }
+    return majorIndex * numberOfMinorColors + minorIndex + 1;
 }
 
-void ToString(ColorPair colorPair, char* buffer, int bufferLen) {
-    // Safely create a string with "Major Minor"
-    snprintf(buffer, bufferLen, "%s %s", MajorColorNames[colorPair.major], MinorColorNames[colorPair.minor]);
+std::string GetReferenceManual() {
+    std::ostringstream oss;
+    oss << "Pair Number | Major Color | Minor Color\n";
+    oss << "-------------------------------------\n";
+    for (int i = 1; i <= numberOfColorPairs; ++i) {
+        ColorPair pair = GetColorFromPairNumber(i);
+        oss << "     " << i << "      | " << pair.majorColor << "       | " << pair.minorColor << "\n";
+    }
+    return oss.str();
 }
+
+}  // namespace ColorCode
